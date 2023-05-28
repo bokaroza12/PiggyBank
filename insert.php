11de3f114ae56085583a5e393a35ee16f654c9cb
Dvergar135 <giorgosnanos98@gmail.com>
2023-05-28 21:27:06 +0300

    new insert

--- insert.php
+++ insert.php
@@ -1,33 +1,17 @@
 <?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
 
-$servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "piggy_db";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Insert data into the 'user' table
-    $sql = "INSERT INTO `user` (`user_id`, `username`, `password`, `email`, `created_at`) VALUES
-            (1, 'john', 'password123', 'john@example.com', NOW()),
-            (2, 'jane', 'pass456', 'jane@example.com', NOW())";
-    $conn->exec($sql);
-
-    // Insert data into the 'category' table
-    $sql = "INSERT INTO `category` (`category_id`, `category_name`) VALUES
-            (1, 'Food'),
-            (2, 'Transportation'),
-            (3, 'Entertainment')";
-    $conn->exec($sql);
-
-    // Insert data into other tables similarly
-
-    echo "Data inserted successfully.";
-} catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'piggy_db');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-
-$conn = null;
 ?>
